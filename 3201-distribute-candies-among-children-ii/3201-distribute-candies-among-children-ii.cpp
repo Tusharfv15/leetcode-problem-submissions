d329078baa4e1fcc0cb8e47c9fb072fb762a027a
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-06-01 17:42:18 +0530

    Time: 32 ms (20%), Space: 8.9 MB (84.71%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long res = 0;
+
+        for(int i = 0;i<=min(limit,n);i++){
+            if(n - i <= 2*limit){
+                res += min(limit,n-i) - max(0,n-i-limit)+1;
+            }
+
+        }
+        return res;
+    }
+};
