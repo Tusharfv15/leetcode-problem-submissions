149c33fa1c0c9aedb366dffe4a207f09d2d0b86f
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-06-03 17:54:34 +0530

    Time: 363 ms (5.02%), Space: 117.6 MB (5.02%) - LeetHub

---
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+int dp[70][70][70][70]; 
+
+
+    int f(int i1, int j1, int i2, int j2, vector<vector<int>>& grid, int n, int m) {
+        if (i1 >= n || j1 < 0 || j1 >= m || i2 >= n || j2 < 0 || j2 >= m)
+            return 0;
+
+            if(dp[i1][j1][i2][j2] !=-1) return dp[i1][j1][i2][j2];
+
+        int maxCherries = 0;
+        for (int dj1 = -1; dj1 <= 1; dj1++) {
+            for (int dj2 = -1; dj2 <= 1; dj2++) {
+                int next_i1 = i1 + 1;
+                int next_j1 = j1 + dj1;
+                int next_i2 = i2 + 1;
+                int next_j2 = j2 + dj2;
+                if(i1 == i2 && j1 == j2) maxCherries = max(maxCherries,  grid[i1][j1]+f(next_i1, next_j1, next_i2, next_j2, grid, n, m));
+                else maxCherries = max(maxCherries,  grid[i1][j1]+grid[i2][j2]+f(next_i1, next_j1, next_i2, next_j2, grid, n, m));
+            }
+        }
+
+       return dp[i1][j1][i2][j2] = maxCherries;
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+         memset(dp, -1, sizeof(dp)); 
+
+        return f(0, 0, 0, m - 1, grid, n, m);
+    }
+};
