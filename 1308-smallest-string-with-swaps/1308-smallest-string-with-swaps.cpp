5bc5c044743f2b620df71d5576b50cc01fc54876
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-06-05 10:25:46 +0530

    Time: 55 ms (41.74%), Space: 60.9 MB (40.24%) - LeetHub

---
+++ 1308-smallest-string-with-swaps/1308-smallest-string-with-swaps.cpp
@@ -0,0 +1,68 @@
+class DisjointSet {
+public:
+    vector<int> parent, size;
+    DisjointSet(int n) {
+        parent.resize(n + 1, 0);
+        size.resize(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUParent(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void Union(int u, int v) {
+        int u_u = findUParent(u);
+        int u_v = findUParent(v);
+
+        if (u_u == u_v)
+            return;
+        if (size[u_u] < size[u_v]) {
+            size[u_v] += size[u_u];
+            parent[u_u] = u_v;
+        } else {
+            size[u_u] += size[u_v];
+            parent[u_v] = u_u;
+        }
+    }
+};
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        int n = s.size();
+        DisjointSet ds(n);
+        for (auto it : pairs) {
+            ds.Union(it[0], it[1]);
+        }
+        map<int, vector<int>> mpp;
+        map<int, string> storeString;
+        for (int i = 0; i < n; i++) {
+            int root = ds.findUParent(i);
+            mpp[root].push_back(i);
+            storeString[root] += s[i];
+        }
+
+     /*    for (auto it : storeString) {
+            sort(it.second.begin(), it.second.end());
+        }
+        for (auto it : mpp) {
+            sort(it.second.begin(), it.second.end());
+        } */
+
+        for (auto& [root, indices] : mpp) {
+            string& chars = storeString[root];
+            sort(chars.begin(), chars.end());
+            sort(indices.begin(), indices.end());
+            for (int i = 0; i < indices.size(); ++i) {
+                s[indices[i]] = chars[i];
+            }
+        }
+
+        return s;
+    }
+};
