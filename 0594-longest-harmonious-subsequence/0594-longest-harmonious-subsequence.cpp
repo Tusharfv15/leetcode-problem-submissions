8864d4def8274a8ec3774664a9b10c31eac828c8
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-06-30 18:41:07 +0530

    Time: 53 ms (10.96%), Space: 45.6 MB (13.3%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return 0;
-        int l = 0;
-        int ans = 0;
-        int r = 1;
-        sort(nums.begin(),nums.end());
-        while(r < n){
-            while(l < r && abs(nums[r] - nums[l]) > 1){
-                l++;
-            }
-            if(nums[r] - nums[l] == 1)ans = max(ans,r-l+1);
-            r++;
+        map<int,int>mpp;
 
+        for(auto it : nums){
+            mpp[it]++;
+        }
+        int ans = 0;
+        for(auto it : mpp){
+            int num = it.first;
+            int freq = it.second;
+            if(mpp.count(num+1)){
+                int len = freq + mpp[num+1];
+                ans = max(ans,len);
+            }
         }
         return ans;
     }
