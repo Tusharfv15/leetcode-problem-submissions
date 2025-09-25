4d96f7913d293203a25c81950d03649a85185e7e
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-09-25 22:13:03 +0530

    Time: 2 ms (16.49%), Space: 12.8 MB (66.61%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[201][201];
+
+    int f(int i, int j, const vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        if (i == n - 1) return triangle[i][j];
+        if (dp[i][j] != INT_MAX) return dp[i][j];
+
+        int down = triangle[i][j] + f(i + 1, j, triangle);
+        int diag = triangle[i][j] + f(i + 1, j + 1, triangle);
+
+        return dp[i][j] = min(down, diag);
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = INT_MAX;
+            }
+        }
+        return f(0, 0, triangle);
+    }
+};
