7a3667e1e9da4d76be7cb0e79fc2f63a06da5ee9
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-09-25 22:17:16 +0530

    Time: 0 ms (100%), Space: 12.8 MB (72.83%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int dp[201][201];
 
-    int f(int i, int j, vector<vector<int>>& triangle) {
+    int f(int i, int j, const vector<vector<int>>& triangle) {
         int n = triangle.size();
         if (i == n - 1) return triangle[i][j];
-        if (dp[i][j] != -1) return dp[i][j];
+        if (dp[i][j] != INT_MIN) return dp[i][j];
 
-        int op1 = triangle[i][j];
-        int op2 = triangle[i][j];
+        int down = triangle[i][j] + f(i + 1, j, triangle);
+        int diag = triangle[i][j] + f(i + 1, j + 1, triangle);
 
-        op1 += f(i + 1, j, triangle);
-        op2 += f(i + 1, j + 1, triangle);
-
-        return dp[i][j] = min(op1, op2);
+        return dp[i][j] = min(down, diag);
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                dp[i][j] = -1;
+                dp[i][j] = INT_MIN;
             }
         }
         return f(0, 0, triangle);
