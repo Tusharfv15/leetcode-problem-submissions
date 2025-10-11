aeaf44578ba1930964a59f87c85c74989189fe9d
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-10-11 15:27:19 +0530

    Time: 425 ms (29.12%), Space: 206.9 MB (58.58%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    vector<long long> dp;
-
-    long long solve(int i, const map<int,int>& mpp, const vector<int>& arr) {
-        int n = arr.size();
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        int j = lower_bound(arr.begin() + i + 1, arr.end(), arr[i] + 3) - arr.begin();
-        long long take = 1LL * mpp.at(arr[i]) * arr[i] + solve(j, mpp, arr);
-        long long notTake = solve(i + 1, mpp, arr);
-
-        return dp[i] = max(take, notTake);
-    }
-
     long long maximumTotalDamage(vector<int>& power) {
         sort(power.begin(), power.end());
         map<int, int> mpp;
@@ -21,8 +7,20 @@ public:
 
         vector<int> arr;
         for (auto &it : mpp) arr.push_back(it.first);
+        int n = arr.size();
 
-        dp.assign(arr.size(), -1); // fix here ✅
-        return solve(0, mpp, arr);
+        vector<long long> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            // find the first index j such that arr[j] >= arr[i] + 3
+            int j = lower_bound(arr.begin() + i + 1, arr.end(), arr[i] + 3) - arr.begin();
+
+            long long take = 1LL * mpp[arr[i]] * arr[i] + dp[j];
+            long long notTake = dp[i + 1];
+
+            dp[i] = max(take, notTake);
+        }
+
+        return dp[0];
     }
 };
