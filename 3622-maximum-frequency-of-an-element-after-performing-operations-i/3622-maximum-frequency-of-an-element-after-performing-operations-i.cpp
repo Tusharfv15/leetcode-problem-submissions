dfc7df0363abad53d50c4df9e831153b6390a471
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-10-22 15:21:54 +0530

    Time: 1042 ms (12.44%), Space: 200 MB (24.39%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         int n = nums.size();
-        int maxEl = *max_element(nums.begin(), nums.end()) + k;
+        int maxEl = *max_element(nums.begin(),nums.end())+k;
 
-        vector<int> storeFreq(maxEl + 1, 0);
-        for (auto it : nums) storeFreq[it]++;
-        for (int i = 1; i <= maxEl; i++) storeFreq[i] += storeFreq[i - 1];
+        map<int,int>freq;
+        map<int,int>diff;
 
-        int ans = 0;
-        for (int i = 0; i <= maxEl; i++) {
-            if (storeFreq[i] == 0) continue;
-            int left = max(0, i - k);
-            int right = min(maxEl, i + k);
+        for(auto it : nums){
+            freq[it]++;
+            int l = max(it-k,0);
+            int r = min(it+k,maxEl);
 
-            int total = storeFreq[right] - (left > 0 ? storeFreq[left - 1] : 0);
-            int targetCount = storeFreq[i] - (i - 1 >= 0 ? storeFreq[i - 1] : 0);
+            diff[l]++;
+            diff[r+1]--;
 
-            int needConversion = total - targetCount;
-            int pos = targetCount + min(needConversion, numOperations);
-            ans = max(ans, pos);
+            diff[it]+=0;
         }
 
-        return ans;
+        int result = 0;
+        int cumSum = 0;
+        for(auto it : diff){
+            int target = it.first;
+            cumSum += it.second;
+
+            int targetFreq = freq[target];
+            int need = cumSum - targetFreq;
+
+            int res = min(need,numOperations);
+
+            result = max(result,res+targetFreq);
+        }
+        return result;
     }
 };
