7cfbbd2838e0a49230502a0c8f641dfb39cd6b9c
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-10-25 14:33:59 +0530

    Time: 0 ms (100%), Space: 7.9 MB (13.49%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int ans = 1;       // day 1: deposit 1
+        int initial = 0;   // weekly increment
+        for (int i = 2; i <= n; i++) {
+            if ((i - 1) % 7 == 0) { // new week starts at day 8, 15, 22, ...
+                initial++;
+            }
+            int val = (i % 7 == 0 ? 7 : i % 7) + initial; // day value
+            ans += val;
+        }
+        return ans;
+    }
+};
