700715d7e0a06be42350f7f15629d98004d7dbbd
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-10-28 19:55:01 +0530

    Time: 177 ms (16.51%), Space: 23.4 MB (22.02%) - LeetHub

---
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int f(int i,vector<int>&nums,int dir){
+        if(i < 0 || i >= nums.size()){
+            for(auto it :nums ){
+                if(it !=0)return 0;
+            }
+            return 1;
+        }
+
+        if(nums[i] == 0) return f(i+dir,nums,dir);
+
+        else {
+            nums[i]--;
+            dir = -dir;
+            return f(i+dir,nums,dir);
+        }
+    }
+    int countValidSelections(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] == 0){
+                vector<int> temp = nums;
+                ans += f(i+1,temp,1);
+                temp = nums;
+                ans += f(i-1,temp,-1);
+            }
+        }
+        return ans;
+    }
+};
