f62f8201c21308c3ea8a6a8b9d884a312b347d4b
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-11-18 22:09:45 +0530

    Time: 0 ms (100%), Space: 13.1 MB (72.06%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        int i = 0;
+        while(i < n){
+            if(i == n-1 && bits[i] == 0)return true;
+            if(bits[i] == 0)i++;
+            else if(bits[i] == 1)i+=2;
+        }
+        return false;
+    }
+};
