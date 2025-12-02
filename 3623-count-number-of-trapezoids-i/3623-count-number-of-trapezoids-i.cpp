0bfb48f6368d9d55b0fb99fd5d3abe47ce59cb9d
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2025-12-02 21:42:38 +0530

    Time: 64 ms (68.68%), Space: 195.4 MB (58.68%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        long long ans = 0;
+        int mod = 1e9 + 7;
+        unordered_map<long long, long long> mpp;
+
+        for (auto &it : points) {
+            mpp[(long long)it[1]]++;
+        }
+
+        long long totPos = 0;
+        for (auto &it : mpp) {
+            long long n = it.second;
+            totPos += (n * (n - 1)) / 2LL;  
+        }
+
+        for (auto &it : mpp) {
+            long long cnt = it.second;
+            long long pos = (cnt*(cnt-1))/2LL;
+            ans += pos * (totPos - pos);
+        }
+
+        ans = ans / 2;  
+
+       
+        ans %= mod;
+        if (ans < 0) ans += mod;
+
+        return (int)ans;
+    }
+};
