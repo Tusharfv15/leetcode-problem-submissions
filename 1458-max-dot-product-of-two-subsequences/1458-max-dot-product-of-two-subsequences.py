8ffe1b8bba1360235d4fa75d7e950ad89b13a612
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2026-01-08 14:44:00 +0530

    Time: 450 ms (10.55%), Space: 22.4 MB (30.15%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -9,10 +9,11 @@ class Solution:
         
         op1 = self.solve(i+1,j,n,m,nums1,nums2,dp)
         op2 = self.solve(i,j+1,n,m,nums1,nums2,dp)
+        op5 = self.solve(i+1,j+1,n,m,nums1,nums2,dp)
         op3 = nums1[i]*nums2[j]
         op4 = nums1[i]*nums2[j] + self.solve(i+1,j+1,n,m,nums1,nums2,dp)
-        dp[i][j] = max(op1,op2,op3,op4)
-        return max(op1,op2,op3,op4)
+        dp[i][j] = max(op1,op2,op3,op4,op5)
+        return max(op1,op2,op3,op4,op5)
 
 
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
