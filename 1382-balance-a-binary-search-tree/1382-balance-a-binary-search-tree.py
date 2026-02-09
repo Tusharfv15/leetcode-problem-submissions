69b5663ef78972530bd83dfd98e7e45a1158d682
Tusharfv15 <136470769+Tusharfv15@users.noreply.github.com>
2026-02-09 17:33:34 +0530

    Time: 38 ms (34.03%), Space: 24.2 MB (46.81%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -1,28 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    
-    def construct_bst(self, store_vals, l, r):
-        if l > r:
+
+    def construct_bst(self,store_vals,l,r):
+        if r < l:
             return None
-        
-        mid = (l + r) // 2
-        
+
+        mid = (r + l)//2
         root = TreeNode(store_vals[mid])
-        root.left = self.construct_bst(store_vals, l, mid - 1)
-        root.right = self.construct_bst(store_vals, mid + 1, r)
-        
+        root.left = self.construct_bst(store_vals,l,mid-1)
+        root.right = self.construct_bst(store_vals,mid+1,r)
         return root
 
-    def traverse(self, store_vals, root):
+    def traverse(self,store_vals,root):
         if not root:
             return
-        
-        self.traverse(store_vals, root.left)
-        store_vals.append(root.val)
-        self.traverse(store_vals, root.right)
 
+        self.traverse(store_vals,root.left)
+        store_vals.append(root.val)
+        self.traverse(store_vals,root.right)
+
+        return
     def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         store_vals = []
+
+        self.traverse(store_vals,root)
+        n = len(store_vals)
+        return self.construct_bst(store_vals,0,n-1)
+
+
+
+
+
+
+
         
-        self.traverse(store_vals, root)
-        
-        return self.construct_bst(store_vals, 0, len(store_vals) - 1)
